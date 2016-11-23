1d95884a695425451e998cdb9585767fb3db0674
ThienLV <ohmygodvt95@gmail.com>
2016-11-23 13:41:39 +0700

    create

---
+++ libs/functions/string_helpper.h
@@ -0,0 +1,14 @@
+//
+// Created by lengkeng on 23/11/2016.
+//
+
+#ifndef SHARE_FILE_STRING_HELPPER_H
+#define SHARE_FILE_STRING_HELPPER_H
+
+#include "../constants.h"
+
+#endif //SHARE_FILE_STRING_HELPPER_H
+
+int is_digit(char c);
+
+int is_alphabet(char c);
