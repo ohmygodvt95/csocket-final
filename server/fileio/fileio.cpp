730f0de00fe759a49827034559fadeeebc58e8ab
ThienLV <ohmygodvt95@gmail.com>
2016-12-07 02:26:53 +0700

    add fileio

---
+++ server/fileio/fileio.cpp
@@ -0,0 +1,5 @@
+//
+// Created by lengkeng on 06/12/2016.
+//
+
+#include "fileio.h"
---
+++ server/fileio/fileio.h
@@ -0,0 +1,14 @@
+//
+// Created by lengkeng on 06/12/2016.
+//
+
+#ifndef SHARE_FILE_FILEIO_H
+#define SHARE_FILE_FILEIO_H
+
+
+class fileio {
+
+};
+
+
+#endif //SHARE_FILE_FILEIO_H
